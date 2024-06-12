52a63970816ee969821549cabcaeb86c812785f4
Ashritha-19 <ashri.vig2337@gmail.com>
2024-06-12 12:54:53 +0530

    Example

---
+++ src/Example/Student.java
@@ -0,0 +1,16 @@
+package Example;
+
+public class Student {
+
+    public String name;
+
+    public String phnNumber;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", phnNumber='" + phnNumber + '\'' +
+                '}';
+    }
+}
---
+++ src/Example/Test.java
@@ -0,0 +1,18 @@
+package Example;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        // Classname reference = new Classname();
+
+
+        s1.name = "Krishna";
+        s1.phnNumber = "1234";
+
+        System.out.println(s1);
+
+    }
+
+}
